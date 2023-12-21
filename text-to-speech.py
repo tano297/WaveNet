ebc99ae27ac06d91880fddcba2a5a15b2d059bcf
someonenameguy <someonenameguy@gmail.com>
2023-12-21 10:58:15 +0800

    imp multiprocessing

--- text-to-speech.py
+++ text-to-speech.py
@@ -3,6 +3,7 @@ import os
 import sys
 import ffmpeg
 import string
+import multiprocessing
 
 # speak stuff
 speak_rate = 1.3
@@ -29,6 +30,10 @@ def synthesize_text(text, client):
 
   return response.audio_content
 
+def synth_text_speech(args):
+    i,line = args
+    audio = synthesize_text(line, client)
+    write_to_file(audio, str(i))
 
 def write_to_file(audio, name):
     # The response's audio_content is binary.
@@ -49,7 +54,8 @@ def concat_oggs(oggs, output):
 def trim_to_nearest_punctuation(input_str):
     # Set of punctuation marks
     punctuation_set = set(string.punctuation)
-
+    if len(input_str.split()) < 20:
+        return input_str
     # Iterate through the characters to find the nearest punctuation
     for i, char in enumerate(input_str):
         if char in punctuation_set:
@@ -93,7 +99,7 @@ if __name__ == '__main__':
   
   # values to be used later
   oggsname=[]
-  title=trim_to_nearest_punctuation(content[0])
+  title=trim_to_nearest_punctuation(content[0]).replace(':',' -')
   
   # create and move in a new folder
   if not os.path.exists(title):
@@ -102,7 +108,11 @@ if __name__ == '__main__':
   
   # download every line of text into a numbered ogg
   for i, line in enumerate(content):
-    audio = synthesize_text(line, client)
-    write_to_file(audio, str(i))
     oggsname.append(str(i) + '.ogg')
+  
+  with multiprocessing.Pool() as pool:
+    pool.map(synth_text_speech, enumerate(content))
+    
   concat_oggs(oggsname, title+'.ogg')
+  
+  
